4f5a1efc2680356b7f500fb4b96c5febecec1832
vayuak <adityakakade7350@gmail.com>
2026-08-09 12:46:24 +0530

    connection

--- src/main/java/com/SocialService/Communities/Clients/UserClient.java
+++ src/main/java/com/SocialService/Communities/Clients/UserClient.java
@@ -5,8 +5,7 @@ import com.SocialService.Communities.Clients.FeignInterceptorConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
-@FeignClient(name = "USER-CATALOG-SERVICE", configuration = FeignInterceptorConfig.class)
+@FeignClient(name = "USER-CATALOG-SERVICE", url = "${user.catalog.service.url}",configuration = FeignInterceptorConfig.class)
 public interface UserClient {
 
     @GetMapping("/api/users/internal/{id}")
