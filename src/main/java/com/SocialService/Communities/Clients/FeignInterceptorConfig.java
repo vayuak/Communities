342c4033f8db3924a1b1a98e7352a3c3e9b4c8cf
vayuak <adityakakade7350@gmail.com>
2026-08-30 18:38:31 +0530

    level 2.1

--- src/main/java/com/SocialService/Communities/Clients/FeignInterceptorConfig.java
+++ src/main/java/com/SocialService/Communities/Clients/FeignInterceptorConfig.java
@@ -1,6 +1,13 @@
 package com.SocialService.Communities.Clients;
 
 import feign.RequestInterceptor;
+import feign.codec.Encoder;
+import feign.form.spring.SpringFormEncoder;
+
+// 🟢 FIX: Added missing ObjectFactory import
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.cloud.openfeign.support.SpringEncoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,5 +21,11 @@ public class FeignInterceptorConfig {
     public RequestInterceptor gatewayShieldInterceptor() {
         // Automatically injects security signatures into outbound microservice loops
         return requestTemplate -> requestTemplate.header(SHIELD_HEADER, SHIELD_SECRET);
+    }
+
+    @Bean
+    public Encoder feignFormEncoder(ObjectFactory<HttpMessageConverters> messageConverters) {
+        // Enables multipart/form-data encoding for cross-service file transfers
+        return new SpringFormEncoder(new SpringEncoder(messageConverters));
     }
 }
