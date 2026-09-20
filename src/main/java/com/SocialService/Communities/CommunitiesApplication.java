3b57591cb1d1343d36644579494fe2e101a2b9a9
vayuak <adityakakade7350@gmail.com>
2026-09-20 17:35:36 +0530

    SUN

--- src/main/java/com/SocialService/Communities/CommunitiesApplication.java
+++ src/main/java/com/SocialService/Communities/CommunitiesApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableCaching
-@EnableFeignClients(basePackages = "com.SocialService.Communities.FeignClients")
+@EnableFeignClients(basePackages = "com.SocialService.Communities.Clients")
 @SpringBootApplication
 @EnableScheduling
 public class CommunitiesApplication {
